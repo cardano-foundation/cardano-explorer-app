0834823a1d2d754a33b7d2ba49ca1edf42a678d1
Sam Jeston <sam.jeston@iohk.io>
2019-08-28 16:05:52 +1000

    Local services reference .env.local

--- source/environment.ts
+++ source/environment.ts
@@ -2,8 +2,8 @@ const isNavigatorDefind = typeof navigator !== 'undefined';
 
 export const environment = {
   DEBUG: process.env.DEBUG,
-  GRAPHQL_HTTP_URL: process.env.GRAPHQL_HTTP_URL,
-  GRAPHQL_WEBSOCKET_URL: process.env.GRAPHQL_WEBSOCKET_URL,
+  GRAPHQL_HTTP_URL: `${process.env.GRAPHQL_API_PROTOCOL}://${process.env.GRAPHQL_API_HOST}:${process.env.GRAPHQL_PORT}/`,
+  GRAPHQL_WEBSOCKET_URL: `${process.env.GRAPHQL_WEBSOCKET_PROTOCOL}://${process.env.GRAPHQL_WEBSOCKET_HOST}:${process.env.GRAPHQL_PORT}/`,
   IS_CLIENT: isNavigatorDefind,
   IS_SERVER: !isNavigatorDefind,
 };
--- utils/start_dependencies.sh
+++ utils/start_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 API_PORT="${API_PORT:-3000}"
+API_HOST="${API_HOST:-'localhost'}"
+API_PROTOCOL="${API_PROTOCOL:-'http'}"
 
 echo "Pulling most recent containers"
 docker-compose pull
@@ -8,14 +10,14 @@ echo "Starting development containers"
 API_PORT=$API_PORT docker-compose up -d
 
 echo "Waiting for GraphQL server to be available..."
-until $(curl --output /dev/null --silent --get http://localhost:${API_PORT}); do
+until $(curl --output /dev/null --silent --get ${API_PROTOCOL}://${API_HOST}:${API_PORT}); do
   sleep 5
 done
 
 echo "API now accepting connections"
 echo "Generating most up to date typings from GraphQL"
-SCHEMA_URI="http://localhost:${API_PORT}" npm run generate:graphql-typings
+SCHEMA_URI="${API_PROTOCOL}://${API_HOST}:${API_PORT}" npm run generate:graphql-typings
 
-echo "GraphQL playground available: http://localhost:${API_PORT}"
+echo "GraphQL playground available: ${API_PROTOCOL}://${API_HOST}:${API_PORT}"
 echo "Ready 🚀"
 
