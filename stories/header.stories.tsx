9c8431c1c73ecd5904003c37ae5b2a88fd70a548
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-09-12 12:18:08 +0200

    [DDW-889] - Implement blocks component

--- stories/header.stories.tsx
+++ stories/header.stories.tsx
@@ -1,6 +1,8 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Header, { BrandType } from '../source/features/shared/header/components';
+import Header, {
+  BrandType,
+} from '../source/features/shared/header/components/Header';
 import { ThemeDecorator } from './support/ThemeDecorator';
 
 storiesOf('Header', module)
