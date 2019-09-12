2b6c67b2c1967bbfe945d1b3a77546651a64ee00
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-09-12 13:58:33 +0200

    [DDW-889] - Implement blocks component - fixing lint errors

--- source/features/shared/epoch/containers/EpochComponentContainer.tsx
+++ source/features/shared/epoch/containers/EpochComponentContainer.tsx
@@ -2,7 +2,6 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import Epoch from '../components/Epoch';
 import { useBlocks } from '../hooks';
-import Address from '../../../address/components/Address';
 
 export const EpochComponentContainerRaw = () => {
   const { actions, store } = useBlocks();
--- source/features/shared/epochs/containers/EpochsComponentContainer.tsx
+++ source/features/shared/epochs/containers/EpochsComponentContainer.tsx
@@ -2,7 +2,6 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import Epochs from '../components/Epochs';
 import { useBlocks } from '../hooks';
-import Address from '../../../address/components/Address';
 
 export const EpochsComponentContainerRaw = () => {
   const { actions, store } = useBlocks();
