2b917dd9b9594bdc6bd617cfca98cb4b95bad912
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-09-13 02:43:39 -0700

    [DDW-896]: Restore back to base structure on header storybook

--- stories/header.stories.tsx
+++ stories/header.stories.tsx
@@ -1,12 +1,8 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Header, {
-  BrandType,
-} from '../source/features/shared/header/components/Header';
+import Header from '../source/features/shared/header/components/Header';
 import { ThemeDecorator } from './support/ThemeDecorator';
 
 storiesOf('Header', module)
   .addDecorator(story => <ThemeDecorator>{story()}</ThemeDecorator>)
-  .add('Header', () => (
-    <Header withBackground withSearchForm brandType={BrandType.ENLARGED} />
-  ));
+  .add('Header', () => <Header title="header" />);
