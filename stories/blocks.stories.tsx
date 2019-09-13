a6bb3cfc52b51259111bdaa3e79ced96ed57533a
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-09-13 02:41:16 -0700

    [DDW-896]: Fix block search form import on storybook

--- stories/blocks.stories.tsx
+++ stories/blocks.stories.tsx
@@ -1,7 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import BlocksSearchForm from '../source/features/blocks/components/BlocksSearchForm';
+import BlocksSearchForm from '../source/features/shared/search/components/Search';
 import { ThemeDecorator } from './support/ThemeDecorator';
 
 storiesOf('Blocks', module)
