1f76f9fc0811449cf1e53590c7bf519d52c35c15
Yakov Karavelov <yakovkaravelov@users.noreply.github.com>
2019-09-16 19:26:20 -0700

    [DDW-887]: Implement header component (#11)

--- source/features/shared/header/containers/HeaderContainer.tsx
+++ source/features/shared/header/containers/HeaderContainer.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-
-import { searchContext } from '../contexts';
-import { blocksContextDefault } from '../index';
 import { HeaderComponentContainer } from './HeaderComponentContainer';
 
-const HeaderContainer = () => (
-  <searchContext.Provider value={blocksContextDefault}>
-    <HeaderComponentContainer />
-  </searchContext.Provider>
-);
+const HeaderContainer = () => <HeaderComponentContainer />;
 
 export default HeaderContainer;
