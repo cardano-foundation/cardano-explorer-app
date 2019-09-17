fcc9daaae5c8c892480e8a515cc7b396ac1836ac
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-09-16 17:39:39 -0700

    [DDW-898]: Adjust header container height on header story

--- stories/header.stories.tsx
+++ stories/header.stories.tsx
@@ -9,7 +9,7 @@ import { ThemeDecorator } from './support/ThemeDecorator';
 storiesOf('Header', module)
   .addDecorator(story => <ThemeDecorator>{story()}</ThemeDecorator>)
   .add('Header', () => (
-    <div style={{ backgroundColor: '#121326', height: '500px' }}>
+    <div style={{ backgroundColor: '#121326', height: '600px' }}>
       <Header withBackground withSearch brandType={BrandType.ENLARGED} />
     </div>
   ));
