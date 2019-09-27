35fbde6545fea4b509378e7b7e919e4df5931037
Danilo Prates <daniloprates@gmail.com>
2019-09-27 12:40:20 +0200

    [DDW-906] Fonts

--- source/styles/index.ts
+++ source/styles/index.ts
@@ -1,1 +1,8 @@
 import './reset.scss';
+// import './variables/variables-fonts.scss';
+import './variables/variables-common.scss';
+
+// TODO: get this variable dinamically
+const themeName = 'incentivized-testnet';
+
+require(`./variables/variables-theme-${themeName}.scss`);
