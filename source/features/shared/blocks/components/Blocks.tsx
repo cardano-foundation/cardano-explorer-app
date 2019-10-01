93dd56999387d7e13a02d9958f28c392ff691ebb
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-01 10:38:38 +0200

    [DDW-889] Implement blocks component - adding blocks component

--- source/features/shared/blocks/components/Blocks.tsx
+++ source/features/shared/blocks/components/Blocks.tsx
@@ -1,14 +1,30 @@
 import { observer } from 'mobx-react-lite';
-
 import styles from './Blocks.scss';
+import BlocksListRow, { IBlocksListRowProps } from './BlocksListRow';
 
-interface IProps {
-  propFirst: string;
-  propSecond: string;
+export interface IBlocksProps {
+  items: Array<IBlocksListRowProps>;
 }
 
-const Blocks = (props: IProps) => {
-  return <div className={styles.root}></div>;
+const Blocks = (props: IBlocksProps) => {
+  return (
+    <div className={styles.blocksListContainer}>
+      <div className={styles.blocksListTitle}>
+        <div className={styles.epoch}>Epoch</div>
+        <div className={styles.blocksSlots}>Block / Slot</div>
+        <div className={styles.createdAt}>Created At</div>
+        <div className={styles.transactions}>Transactions</div>
+        <div className={styles.output}>Output (₳)</div>
+        <div className={styles.size}>Size (Bytes)</div>
+        <div className={styles.createdBy}>Created By</div>
+      </div>
+      {props.items.map((item, index) => (
+        <div key={`epoch_${index}`} className={styles.blocksListRow}>
+          <BlocksListRow {...item} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default observer(Blocks);
