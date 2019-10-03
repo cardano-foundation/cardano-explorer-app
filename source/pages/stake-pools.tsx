cfe18aac86857ae0c705dd762a5f48824b4bff76
Danilo Prates <daniloprates@gmail.com>
2019-10-03 08:49:32 -0300

    [DDW-913] Re-using EpochInfo

--- source/pages/stake-pools.tsx
+++ source/pages/stake-pools.tsx
@@ -2,6 +2,7 @@ import debug from 'debug';
 import React from 'react';
 import NoSSR from 'react-no-ssr';
 import { environment } from '../environment';
+import EpochInfo from '../features/epochs/components/EpochInfo';
 
 if (environment.DEBUG) {
   debug.enable(environment.DEBUG);
@@ -18,9 +19,18 @@ if (environment.IS_CLIENT) {
 
   EpochPage = () => (
     <NoSSR>
-      <Layout>
+      <Layout hasContainer>
         <HeaderContainer />
-        STAKE POOLS LIST!
+        <EpochInfo
+          blocks={21073}
+          endedAt={1569144483000}
+          epoch={135}
+          output={8397621.461829}
+          slots={21600}
+          startedAt={1568366883000}
+          status="In progress..."
+          transactions={12}
+        />
         <FooterContainer />
       </Layout>
     </NoSSR>
