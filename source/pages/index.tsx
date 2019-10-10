4bca2883b5901bcb215d83708ada557f9263c9a5
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-10-10 13:02:17 -0700

    [DDW-863]: Fix main page

--- source/pages/index.tsx
+++ source/pages/index.tsx
@@ -6,10 +6,6 @@ import BlockList from '../features/blocks/components/BlockList';
 import { IBlockListRowProps } from '../features/blocks/components/BlockListRow';
 import EpochList from '../features/epochs/components/EpochList';
 import { IEpochListRowProps } from '../features/epochs/components/EpochListRow';
-import Footer from '../features/widgets/footer/components/Footer';
-import Header, {
-  BrandType,
-} from '../features/widgets/header/components/Header';
 import styles from './index.scss';
 
 export interface IMainPageProps {
@@ -17,16 +13,126 @@ export interface IMainPageProps {
   blocks: Array<IBlockListRowProps>;
 }
 
-const Layout = require('../layout/Layout').default;
-const SideBackgroundImage = require('../static/assets/images/main-side-background.svg');
-
 if (environment.DEBUG) {
   debug.enable(environment.DEBUG);
 }
 
+const blocks = [
+  {
+    block: 20051,
+    createdAt: 1568366883000,
+    createdBy: 'af2800c',
+    epoch: 138,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 1,
+  },
+  {
+    block: 20051,
+    createdAt: 1568366883000,
+    createdBy: '6c9e149',
+    epoch: 139,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 2,
+  },
+  {
+    block: 20051,
+    createdAt: 1568366883000,
+    createdBy: 'af2800c',
+    epoch: 137,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 3,
+  },
+  {
+    block: 20051,
+    createdAt: 1568366883000,
+    createdBy: '6c9e149',
+    epoch: 139,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 4,
+  },
+  {
+    block: 20051,
+    createdAt: 1568366883000,
+    createdBy: 'e1496c9',
+    epoch: 140,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 5,
+  },
+];
+
+const epochs = [
+  {
+    blocks: 20051,
+    epoch: 138,
+    output: 11189.647356,
+    percentage: 40,
+    slots: 21600,
+    startedAt: 1568366883000,
+    status: 'In progress...',
+    transactions: 1,
+  },
+  {
+    blocks: 21045,
+    endedAt: 1569144483000,
+    epoch: 137,
+    output: 0.0,
+    slots: 21600,
+    startedAt: 1568366883000,
+    status: '',
+    transactions: 17,
+  },
+  {
+    blocks: 21367,
+    endedAt: 1569144483000,
+    epoch: 136,
+    output: 70090.386627,
+    slots: 21600,
+    startedAt: 1568366883000,
+    status: '',
+    transactions: 0,
+  },
+  {
+    blocks: 21073,
+    endedAt: 1569144483000,
+    epoch: 135,
+    output: 8397621.461829,
+    slots: 21600,
+    startedAt: 1568366883000,
+    status: '',
+    transactions: 12,
+  },
+  {
+    blocks: 20034,
+    endedAt: 1569144483000,
+    epoch: 134,
+    output: 18.872021,
+    slots: 21600,
+    startedAt: 1568366883000,
+    status: '',
+    transactions: 5,
+  },
+];
+
 let IndexPage = (props: IMainPageProps) => <NoSSR />;
 
 if (environment.IS_CLIENT) {
+  const Layout = require('../layout/Layout').default;
+  const SideBackgroundImage = require('../static/assets/images/main-side-background.svg');
+  const HeaderContainer = require('../features/widgets/header/containers/HeaderContainer')
+    .default;
+  const FooterContainer = require('../features/widgets/footer/containers/FooterContainer')
+    .default;
+
   IndexPage = (props: IMainPageProps) => (
     <NoSSR>
       <Layout hasContainer>
@@ -41,14 +147,14 @@ if (environment.IS_CLIENT) {
         <div className={styles.sideBackgroundImageContainer}>
           <SideBackgroundImage className={styles.sideBackgroundImage} />
         </div>
-        <Header withSearch brandType={BrandType.ENLARGED} />
+        <HeaderContainer />
         <div className={styles.epochList}>
-          <EpochList title="Latest Epochs" items={props.epochs} />
+          <EpochList title="Latest Epochs" items={props.epochs || epochs} />
         </div>
         <div className={styles.blockList}>
-          <BlockList title="Latest Blocks" items={props.blocks} />
+          <BlockList title="Latest Blocks" items={props.blocks || blocks} />
         </div>
-        <Footer propFirst="" propSecond="" />
+        <FooterContainer />
       </Layout>
     </NoSSR>
   );
