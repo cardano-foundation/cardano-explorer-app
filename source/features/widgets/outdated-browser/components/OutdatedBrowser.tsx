16e339144c7212ba1b56b6dc35d8b0456e2d2e19
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-13 23:59:51 +0200

    [DDW-859] Unsupported browser notification - fixes

--- source/features/widgets/outdated-browser/components/OutdatedBrowser.tsx
+++ source/features/widgets/outdated-browser/components/OutdatedBrowser.tsx
@@ -1,5 +1,4 @@
-import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './OutdatedBrowser.scss';
 
 const CardanoLogo = require('../../../../static/assets/images/header/cardano-logo.svg');
@@ -8,39 +7,93 @@ export interface IOutdatedBrowserProps {
   text: string;
 }
 
-const OutdatedBrowser = (props: IOutdatedBrowserProps) => {
-  const { text } = props;
+export default class OutdatedBrowser extends Component<IOutdatedBrowserProps> {
+  public detectUserBrowser = () => {
+    // @ts-ignore
+    const isOpera =
+      (!!window.opr && !!opr.addons) ||
+      !!window.opera ||
+      navigator.userAgent.indexOf(' OPR/') >= 0;
 
-  const userBrowser = window.navigator.appName;
+    // @ts-ignore
+    const isFirefox = typeof InstallTrigger !== 'undefined';
 
-  return (
-    <div className={styles.outdatedBrowserContainer}>
-      <div className={styles.topHeaderContainer}>
-        <div className={styles.contentContainer}>
-          <div className={styles.brandType}>
-            <div className={styles.logoContainer}>
-              <CardanoLogo className={styles.logo} />
-            </div>
-            <div className={styles.titleContainer}>
-              <span className={styles.outdatedTitle}>{text}</span>
-            </div>
-            <div className={styles.triangleSign}>
-              <div className={styles.straightLine} />
-              <div className={styles.triangle}>
-                <div className={styles.innerTriangle} />
+    // @ts-ignore
+    const isSafari =
+      /constructor/i.test(window.HTMLElement) ||
+      (p => {
+        return p.toString() === '[object SafariRemoteNotification]';
+        // @ts-ignore
+      })(
+        !window.safari ||
+          (typeof safari !== 'undefined' && safari.pushNotification)
+      );
+
+    // @ts-ignore
+    const isIE = /*@cc_on!@*/ false || !!document.documentMode;
+
+    // @ts-ignore
+    const isEdge = !isIE && !!window.StyleMedia;
+
+    // @ts-ignore
+    const isChrome =
+      !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+    // @ts-ignore
+    const isBlink = (isChrome || isOpera) && !!window.CSS;
+
+    let browserType = '';
+
+    if (isOpera) {
+      browserType = 'Opera';
+    } else if (isFirefox) {
+      browserType = 'Firefox';
+    } else if (isSafari) {
+      browserType = 'Safari';
+    } else if (isIE) {
+      browserType = 'Internet Explorer';
+    } else if (isEdge) {
+      browserType = 'EDGE';
+    } else if (isChrome) {
+      browserType = 'Chrome';
+    } else if (isBlink) {
+      browserType = 'Blink';
+    }
+    return browserType;
+  };
+
+  public render() {
+    const { text } = this.props;
+
+    const userBrowser = this.detectUserBrowser();
+
+    return (
+      <div className={styles.outdatedBrowserContainer}>
+        <div className={styles.topHeaderContainer}>
+          <div className={styles.contentContainer}>
+            <div className={styles.brandType}>
+              <div className={styles.logoContainer}>
+                <CardanoLogo className={styles.logo} />
+              </div>
+              <div className={styles.titleContainer}>
+                <span className={styles.outdatedTitle}>{text}</span>
+              </div>
+              <div className={styles.triangleSign}>
+                <div className={styles.straightLine} />
+                <div className={styles.triangle}>
+                  <div className={styles.innerTriangle} />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className={styles.bottomContainer}>
+          <p className={styles.bottomContainerText}>
+            Update your {userBrowser && <span>({userBrowser})</span>} browser
+            for more security, speed and the best experience on this site.
+          </p>
+        </div>
       </div>
-      <div className={styles.bottomContainer}>
-        <p className={styles.bottomContainerText}>
-          Update your ({userBrowser}) browser for more security, speed and the
-          best experience on this site.
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default observer(OutdatedBrowser);
+    );
+  }
+}
