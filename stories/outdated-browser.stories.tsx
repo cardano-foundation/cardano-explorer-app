14a00ecb93dbeca075fa6974d9a7a5a445f598b9
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-14 01:34:28 +0200

    [DDW-859] Unsupported browser notification - updates for logos

--- stories/outdated-browser.stories.tsx
+++ stories/outdated-browser.stories.tsx
@@ -3,5 +3,8 @@ import React from 'react';
 import OutdatedBrowser from '../source/features/widgets/outdated-browser/components/OutdatedBrowser';
 
 storiesOf('Outdated Browser', module).add('Outdated Browser', () => (
-  <OutdatedBrowser />
+  <OutdatedBrowser
+    text="Your web browser is out of date"
+    updateBrowserUrl="http://browser-update.org/update.html?force_outdated=true"
+  />
 ));
