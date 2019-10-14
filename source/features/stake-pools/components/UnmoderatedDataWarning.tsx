a664e91ad1fde903c391a42a84beeffbd6db8636
Danilo Prates <daniloprates@gmail.com>
2019-10-14 19:57:00 -0300

    [DDW-914] Styling progress

--- source/features/stake-pools/components/UnmoderatedDataWarning.tsx
+++ source/features/stake-pools/components/UnmoderatedDataWarning.tsx
@@ -1,9 +1,33 @@
 import styles from './UnmoderatedDataWarning.scss';
+import Link from 'next/link';
 import { IUnmoderatedDataWarning } from '../types';
+import Container from '../../widgets/container/components/Container';
 
 export default ({ onAcceptUnmoderatedData }: IUnmoderatedDataWarning) => (
   <div className={styles.unmoderatedDataWarningContainer}>
-    NOOUPE
-    <button onClick={onAcceptUnmoderatedData}>accept</button>
+    <Container>
+      <div className={styles.contentTop}>
+        <h2>
+          <b>Warning:</b> Stake pool data is not moderated
+        </h2>
+        <p>
+          Stake pool ticker symbols, titles, descriptions and websites are not
+          moderated and may contain inappropriate content.
+        </p>
+        <p>
+          This information is provided and hosted by stake pool operators and it
+          is not in control of entities running this website.
+        </p>
+        <p>Do you want to see unmoderated content?</p>
+      </div>
+      <div className={styles.contentBottom}>
+        <Link href="/">
+          <a>Leave this page</a>
+        </Link>
+        <button onClick={onAcceptUnmoderatedData}>
+          Yes, show unmoderated content
+        </button>
+      </div>
+    </Container>
   </div>
 );
