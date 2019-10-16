82b0654d8296b27cd9e2f14d1d54c86ba6bb1d0f
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-10-15 18:31:11 -0700

    [DDW-865]: Update story decorator and related stories

--- stories/support/ThemeDecorator.tsx
+++ stories/support/ThemeDecorator.tsx
@@ -4,16 +4,15 @@ import PolymorphThemeProvider from '../../source/styles/theme/PolymorphThemeProv
 import '../../source/styles/global/_fonts.scss';
 import '../../source/styles/global/_reset.scss';
 import './global.scss';
+
 import styles from './ThemeDecorator.scss';
 
 interface IProps {
   children: React.ReactNode;
 }
 
-export const ThemeDecorator = ({ children }: IProps) => {
-  return (
-    <PolymorphThemeProvider>
-      <div className={styles.storyContainer}>{children}</div>
-    </PolymorphThemeProvider>
-  );
-};
+export const ThemeDecorator = ({ children }: IProps) => (
+  <PolymorphThemeProvider>
+    <div className={styles.storyContainer}>{children}</div>
+  </PolymorphThemeProvider>
+);
