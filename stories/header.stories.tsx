ff984a9f066990f99b51259040d244e856e2cbf5
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-10-15 17:31:23 -0700

    [DDW-865]: Fix constant import

--- stories/header.stories.tsx
+++ stories/header.stories.tsx
@@ -1,8 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Header, {
-  BrandType,
-} from '../source/features/widgets/header/components/Header';
+import { BrandType } from '../source/common/constants';
+import Header from '../source/features/widgets/header/components/Header';
 
 storiesOf('Header', module).add('Header', () => (
   <Header withSearch brandType={BrandType.ENLARGED} />
