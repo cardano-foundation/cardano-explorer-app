f7bd7ca3ee028aee7ce5c744e59e26982f62fadd
Danilo <daniloprates@gmail.com>
2019-10-17 10:42:08 -0300

    [DDW-914] DDW-914 Implement 'Unmoderated stake pool data warning' notification (#28)
    * [DDW-914] Basic logic
    
    * [DDW-914] 24h logic
    
    * [DDW-914] Separates the Container from the Layout
    
    * [DDW-914] Styling progress
    
    * [DDW-914] Mobx bindings and lint errors
    
    * [DDW-914] Consent given banner
    
    * [DDW-914] Storybook stories
    
    * [DDW-914] Removes 'hide unmoderated data' button
    
    * [DDW-914] Correct Network version for Storybook

---
+++ source/features/stake-pools/components/UnmoderatedDataConsented.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+import Container from '../../widgets/container/components/Container';
+import styles from './UnmoderatedDataConsented.scss';
+
+export default () => (
+  <div className={styles.unmoderatedDataConsentedContainer}>
+    You have consented to view unmoderated stake pool data.
+  </div>
+);
--- source/features/stake-pools/containers/StakePoolsContainer.tsx
+++ source/features/stake-pools/containers/StakePoolsContainer.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { stakePoolsContext } from '../contexts';
-import { blocksContextDefault } from '../index';
+import { stakePoolsContextDefault } from '../index';
 import { StakePoolsComponentContainer } from './StakePoolsComponentContainer';
 
 const StakePoolsContainer = () => (
-  <stakePoolsContext.Provider value={blocksContextDefault}>
+  <stakePoolsContext.Provider value={stakePoolsContextDefault}>
     <StakePoolsComponentContainer />
   </stakePoolsContext.Provider>
 );
