59f1d892c7642e8f5e6c8d54bba19d6a4c22d749
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-17 16:28:09 +0200

    [DDW-917] Add block creation table to the previous epoch page

--- source/pages/epoch.tsx
+++ source/pages/epoch.tsx
@@ -1,26 +1,103 @@
 import debug from 'debug';
 import React from 'react';
 import NoSSR from 'react-no-ssr';
+import { BrandType } from '../common/constants';
 import { environment } from '../environment';
+import BlockList from '../features/blocks/components/BlockList';
+import EpochSummary from '../features/epochs/components/EpochSummary';
+import FooterContainer from '../features/widgets/footer/containers/FooterContainer';
+import Header from '../features/widgets/header/components/Header';
+import Layout from '../layout/Layout';
+import styles from './epoch.scss';
 
 if (environment.DEBUG) {
   debug.enable(environment.DEBUG);
 }
 
+const epochSummary = {
+  blocks: 21490,
+  endedAt: 1579366885000,
+  epoch: 48,
+  output: 0.0,
+  percentage: 40,
+  slots: 21600,
+  startedAt: 1568366883000,
+  status: 'In progress...',
+  title: 'Epoch',
+  transactions: 0,
+};
+
+const blocks = [
+  {
+    block: 21490,
+    createdAt: 1568366883000,
+    createdBy: 'af2800c',
+    epoch: 48,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 1,
+  },
+  {
+    block: 21490,
+    createdAt: 1568366883000,
+    createdBy: '6c9e149',
+    epoch: 48,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 2,
+  },
+  {
+    block: 21490,
+    createdAt: 1568366883000,
+    createdBy: 'af2800c',
+    epoch: 48,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 3,
+  },
+  {
+    block: 21490,
+    createdAt: 1568366883000,
+    createdBy: '6c9e149',
+    epoch: 48,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 4,
+  },
+  {
+    block: 21490,
+    createdAt: 1568366883000,
+    createdBy: 'e1496c9',
+    epoch: 48,
+    output: 11189.647356,
+    size: 1024,
+    slots: 21600,
+    transactions: 5,
+  },
+];
+
 let EpochPage = () => <NoSSR />;
 
 if (environment.IS_CLIENT) {
-  const Layout = require('../layout/Layout').default;
-  const HeaderContainer = require('../features/widgets/header/containers/HeaderContainer')
-    .default;
-  const FooterContainer = require('../features/widgets/footer/containers/FooterContainer')
-    .default;
-
   EpochPage = () => (
     <NoSSR>
       <Layout>
-        <HeaderContainer />
-        <FooterContainer />
+        <div className={styles.epochLayout}>
+          <Header withSearch brandType={BrandType.SHRINKED} />
+          <div className={styles.container}>
+            <div className={styles.epochSummary}>
+              <EpochSummary {...epochSummary} />
+            </div>
+            <div className={styles.blockList}>
+              <BlockList title="Blocks" items={blocks} />
+            </div>
+          </div>
+          <FooterContainer />
+        </div>
       </Layout>
     </NoSSR>
   );
