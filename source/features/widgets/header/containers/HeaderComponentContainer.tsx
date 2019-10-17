7c1a5eeaf418e68a266c6284705fcf3dc795d12c
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-10-17 10:19:52 -0700

    [DDW-935]: Decouple search component from search blocks feature

--- source/features/widgets/header/containers/HeaderComponentContainer.tsx
+++ source/features/widgets/header/containers/HeaderComponentContainer.tsx
@@ -1,10 +1,17 @@
+import noop from 'lodash';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { BrandType } from '../../../../common/constants';
 import Header from '../components/Header';
 
 export const HeaderComponentContainerRaw = () => {
-  return <Header withSearch brandType={BrandType.ENLARGED} />;
+  return (
+    <Header
+      withSearch
+      brandType={BrandType.ENLARGED}
+      searchProps={{ onSearch: noop }}
+    />
+  );
 };
 
 export const HeaderComponentContainer = observer(HeaderComponentContainerRaw);
--- stories/search.stories.tsx
+++ stories/search.stories.tsx
@@ -1,5 +1,5 @@
-import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
+import { noop } from 'lodash';
 import React from 'react';
 import NoSearchResult from '../source/features/widgets/search/components/NoSearchResult';
 import Search from '../source/features/widgets/search/components/Search';
@@ -7,7 +7,5 @@ import { PaddingDecorator } from './support/PaddingDecorator';
 
 storiesOf('Search', module)
   .addDecorator(story => <PaddingDecorator>{story()}</PaddingDecorator>)
-  .add('Search', () => (
-    <Search triggerBlockSearch={action('triggerBlockSearch')} />
-  ))
+  .add('Search', () => <Search onSearch={noop} />)
   .add('No Search Result', () => <NoSearchResult />);
