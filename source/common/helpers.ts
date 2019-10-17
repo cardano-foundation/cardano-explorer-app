042a4fce7869ec7ed9f780c1fcf19d9e8a221bbd
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-10-17 02:38:21 -0700

    [DDW-865]: Fix address ellipsis styles on transaction info panel

--- source/common/helpers.ts
+++ source/common/helpers.ts
@@ -1,12 +1,3 @@
 import { SCREEN_BREAKPOINTS } from './constants';
 
-export const shrinkAddress = (address: string) => {
-  if (address.length <= 34) {
-    return address;
-  }
-  return `${address.substring(0, 17)}...${address.substring(
-    address.length - 17
-  )}`;
-};
-
 export const isMobileScreen = () => screen.width < SCREEN_BREAKPOINTS.XS;
