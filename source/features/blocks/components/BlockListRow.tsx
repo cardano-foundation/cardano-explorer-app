38c7afbe8ffe7ffd0faed959d3f968888b42758e
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-18 10:03:16 +0200

    [DDW-917] Add block creation table to the previous epoch page - fixes for blocks list container visibility (show/hide)

--- source/features/blocks/components/BlockListRow.tsx
+++ source/features/blocks/components/BlockListRow.tsx
@@ -1,5 +1,5 @@
-import { observer } from 'mobx-react-lite';
 import moment from 'moment';
+import React, { Component } from 'react';
 import styles from './BlockListRow.scss';
 
 export interface IBlockListRowProps {
@@ -13,21 +13,48 @@ export interface IBlockListRowProps {
   transactions: number;
 }
 
-const BlockListRow = (props: IBlockListRowProps) => (
-  <div className={styles.blockListRowContainer}>
-    <div className={styles.epoch}>{props.epoch}</div>
-    <div className={styles.blocksSlots}>
-      {props.block}
-      <span className={styles.blocksSlotsIcon} />
-    </div>
-    <div className={styles.createdAt}>
-      {moment(props.createdAt).format('YYYY/MM/DD HH:mm:ss')}
-    </div>
-    <div className={styles.transactions}>{props.transactions}</div>
-    <div className={styles.output}>{props.output}</div>
-    <div className={styles.size}>{props.size}</div>
-    <div className={styles.createdBy}>{props.createdBy}</div>
-  </div>
-);
+interface IState {
+  showBlocksSlotsIcon: boolean;
+}
+const initialState = {
+  showBlocksSlotsIcon: true,
+};
 
-export default observer(BlockListRow);
+export default class BlockListRow extends Component<
+  IBlockListRowProps,
+  IState
+> {
+  public state = {
+    ...initialState,
+  };
+
+  public render() {
+    const { showBlocksSlotsIcon } = this.state;
+    const {
+      block,
+      createdAt,
+      createdBy,
+      epoch,
+      output,
+      size,
+      transactions,
+    } = this.props;
+
+    return (
+      <div className={styles.blockListRowContainer}>
+        <div className={styles.epoch}>{epoch}</div>
+        <div className={styles.blocksSlots}>
+          {block}
+          {showBlocksSlotsIcon && <span className={styles.blocksSlotsIcon} />}
+        </div>
+        <div className={styles.createdAt}>
+          {moment(createdAt).format('YYYY/MM/DD HH:mm:ss')}
+        </div>
+        <div className={styles.transactions}>{transactions}</div>
+        <div className={styles.output}>{output}</div>
+        <div className={styles.size}>{size}</div>
+        <div className={styles.createdBy}>{createdBy}</div>
+      </div>
+    );
+  }
+}
