e5ab26f0e01cab4ba8b8e8fbe6eed07cb0b990ca
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-21 22:25:12 +0200

    [DDW-917] Add block creation table to the previous epoch page - fixes for block creation table and items

---
+++ source/features/blocks/components/BlockCreation.tsx
@@ -0,0 +1,31 @@
+import { observer } from 'mobx-react-lite';
+import { Button } from 'react-polymorph/lib/components/Button';
+import DividerWithTitle from '../../widgets/divider-with-title/components/DividerWithTitle';
+import styles from './BlockCreation.scss';
+import BlockCreationRow, { IBlockCreationRowProps } from './BlockCreationRow';
+
+export interface IBlockCreationProps {
+  title: string;
+  items: Array<IBlockCreationRowProps>;
+}
+
+const BlockCreation = (props: IBlockCreationProps) => (
+  <div className={styles.blockCreationContainer}>
+    <div className={styles.header}>
+      <DividerWithTitle title={props.title} />
+    </div>
+    <div className={styles.listHeader}>
+      <div className={styles.stakePool}>Stake pool</div>
+      <div className={styles.slotsElected}>Slots elected</div>
+      <div className={styles.performance}>Performance</div>
+      <div className={styles.sharedRewards}>Shared rewards</div>
+    </div>
+    {props.items.map((item, index) => (
+      <div key={`epoch_${index}`} className={styles.blockListRow}>
+        <BlockCreationRow {...item} />
+      </div>
+    ))}
+  </div>
+);
+
+export default observer(BlockCreation);
