0e0240708e88f20283e8c4e47c785f27c22cbf0c
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-23 20:04:25 +0200

    [DDW-917] Add block creation table to the previous epoch page - fix

--- source/pages/error.tsx
+++ source/pages/error.tsx
@@ -2,6 +2,10 @@ import debug from 'debug';
 import React from 'react';
 import NoSSR from 'react-no-ssr';
 import { environment } from '../environment';
+import FooterContainer from '../features/widgets/footer/containers/FooterContainer';
+import HeaderContainer from '../features/widgets/header/containers/HeaderContainer';
+import NoSearchResult from '../features/widgets/search/components/NoSearchResult';
+import Layout from '../layout/Layout';
 
 if (environment.DEBUG) {
   debug.enable(environment.DEBUG);
@@ -10,14 +14,6 @@ if (environment.DEBUG) {
 let ErrorPage = () => <NoSSR />;
 
 if (environment.IS_CLIENT) {
-  const Layout = require('../layout/Layout').default;
-  const HeaderContainer = require('../features/widgets/header/containers/HeaderContainer')
-    .default;
-  const NoSearchResult = require('../features/widgets/search/components/NoSearchResult')
-    .default;
-  const FooterContainer = require('../features/widgets/footer/containers/FooterContainer')
-    .default;
-
   ErrorPage = () => (
     <NoSSR>
       <Layout hasContainer>
