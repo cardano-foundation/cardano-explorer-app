cb23bf9dc5d545bdc2187242e924217f88741f50
Dominik Guzei <dominik.guzei@gmail.com>
2019-10-23 17:30:50 +0200

    [DDW-990] Integrate new epochs page

--- stories/pages.stories.tsx
+++ stories/pages.stories.tsx
@@ -2,6 +2,7 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { IndexPage as ByronIndexPage } from '../source/apps/byron/index';
 import { AddressPage } from '../source/apps/shared/address';
+import { EpochPage } from '../source/apps/shared/epoch';
 import { NoSearchResultsPage } from '../source/apps/shared/no-search-results';
 import { OutdatedBrowserPage } from '../source/apps/shared/outdated-browser';
 import { IndexPage as ShelleyIndexPage } from '../source/apps/shelley/index';
@@ -11,11 +12,10 @@ storiesOf('Pages|Shelley Client', module)
   .add('Index', () => <ShelleyIndexPage />)
   .add('Stake Pools', () => <StakePoolsPage />);
 
-storiesOf('Pages|Byron Client', module).add('Index', () => (
-  <ByronIndexPage />
-));
+storiesOf('Pages|Byron Client', module).add('Index', () => <ByronIndexPage />);
 
 storiesOf('Pages|Shared', module)
   .add('Address', () => <AddressPage />)
   .add('No Search Results', () => <NoSearchResultsPage />)
-  .add('Outdated Browser', () => <OutdatedBrowserPage />);
+  .add('Outdated Browser', () => <OutdatedBrowserPage />)
+  .add('Epoch', () => <EpochPage />);
