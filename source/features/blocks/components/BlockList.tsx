63a1478008c17ea47ac7857761a808ae74b2f1a4
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-10-27 15:17:24 -0700

    [DDW-941]: Update block list component using global table component

--- source/features/blocks/components/BlockList.tsx
+++ source/features/blocks/components/BlockList.tsx
@@ -1,40 +1,69 @@
 import { observer } from 'mobx-react-lite';
-import { Button } from 'react-polymorph/lib/components/Button';
-import DividerWithTitle from '../../widgets/divider-with-title/components/DividerWithTitle';
+import moment from 'moment';
+import React, { FC } from 'react';
+import Table from '../../widgets/table/components/Table';
+import { IColumnDefinition } from '../../widgets/table/types/Table';
 import styles from './BlockList.scss';
-import BlockListRow, { IBlockListRowProps } from './BlockListRow';
+
+export interface IBlockListRowProps {
+  block: number;
+  createdAt: number;
+  createdBy: string;
+  epoch: number;
+  output: number;
+  size: number;
+  slots: number;
+  transactions: number;
+}
 
 export interface IBlockListProps {
   title: string;
   items: Array<IBlockListRowProps>;
 }
 
-const BlockList = (props: IBlockListProps) => (
+const columns: Array<IColumnDefinition<IBlockListRowProps>> = [
+  {
+    cssClass: 'epoch',
+    head: 'Epoch',
+    key: 'epoch',
+  },
+  {
+    cssClass: 'blocksSlots',
+    head: 'Block / Slot',
+    key: 'block',
+  },
+  {
+    cellValue: (row: IBlockListRowProps) =>
+      moment(row.createdAt).format('YYYY/MM/DD HH:mm:ss'),
+    cssClass: 'createdAt',
+    head: 'Created At',
+    key: 'createdAt',
+  },
+  {
+    cssClass: 'transactions',
+    head: 'Transactions',
+    key: 'transactions',
+  },
+  {
+    cssClass: 'output',
+    head: 'Output (₳)',
+    key: 'output',
+  },
+  {
+    cssClass: 'size',
+    head: 'Size (Bytes)',
+    key: 'size',
+  },
+  {
+    cssClass: 'createdBy',
+    head: 'Created By',
+    key: 'createdBy',
+  },
+];
+
+const BlockList: FC<IBlockListProps> = ({ title, items }) => (
   <div className={styles.blockListContainer}>
-    <div className={styles.header}>
-      <DividerWithTitle title={props.title} />
-    </div>
-    <div className={styles.listHeader}>
-      <div className={styles.epoch}>Epoch</div>
-      <div className={styles.blocksSlots}>Block / Slot</div>
-      <div className={styles.createdAt}>Created At</div>
-      <div className={styles.transactions}>Transactions</div>
-      <div className={styles.output}>Output (₳)</div>
-      <div className={styles.size}>Size (Bytes)</div>
-      <div className={styles.createdBy}>Created By</div>
-    </div>
-    {props.items.map((item, index) => (
-      <div key={`epoch_${index}`} className={styles.blockListRow}>
-        <BlockListRow {...item} />
-      </div>
-    ))}
-    <div className={styles.showMore}>
-      <Button
-        className={styles.showMoreButton}
-        label="Show more blocks"
-        onClick={() => null}
-      />
-    </div>
+    <Table title={title} columns={columns} rows={items} withShowMore />
   </div>
 );
 
