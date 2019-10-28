9bd6b6d989c1ade73200aabf19bdab78ec07eca0
Dominik Guzei <dominik.guzei@gmail.com>
2019-10-28 16:14:22 +0100

    [DDW-1020] Fetch all available transaction data

--- source/features/search/specs/search.spec.ts
+++ source/features/search/specs/search.spec.ts
@@ -2,8 +2,8 @@ import { searchActions, searchApi, searchStore } from '../index';
 
 describe('Searching', () => {
   describe('transactions', () => {
-    describe('by complete valid ID', () => {
-      it('should ', async () => {
+    describe('by complete valid transaction ID', () => {
+      it('retrieves the transaction with expected data', async () => {
         const searchedTxId =
           '927edb96f3386ab91b5f5d85d84cb4253c65b1c2f65fa7df25f81fab1d62987a';
         searchActions.searchTransactionById.trigger({
@@ -16,7 +16,24 @@ describe('Searching', () => {
           const foundTx = transactions[0];
           expect(transactions).toHaveLength(1);
           expect(foundTx).toMatchObject({
+            block: {
+              id:
+                '5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb',
+            },
+            fee: '0',
             id: searchedTxId,
+            inputs: [],
+            outputs: [
+              {
+                address:
+                  'Ae2tdPwUPEZ9vtyppa1FdJzvqJZkEcXgdHxVYAzTWcPaoNycVq5rc36LC1S',
+                index: 0,
+                txId:
+                  '927edb96f3386ab91b5f5d85d84cb4253c65b1c2f65fa7df25f81fab1d62987a',
+                value: '538861000000',
+              },
+            ],
+            totalOutput: '538861000000',
           });
           expect(searchStore.searchedTransaction).toEqual(foundTx);
         } else {
@@ -26,8 +43,8 @@ describe('Searching', () => {
     });
   });
   describe('blocks', () => {
-    describe('by id', () => {
-      it('retrieves the block with given id', async () => {
+    describe('by complete valid block ID', () => {
+      it('retrieves the block with expected data', async () => {
         const searchedBlockId =
           '84e9de7924aba73f58b81e142f4bce7f1d00cf4630f94f631e6ca3594b2d1634';
         searchActions.searchBlockById.trigger({
