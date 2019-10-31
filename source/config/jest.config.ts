7d6ac213f979e570926522ce94e78bd4abfaf1ea
Rhys Bartlels-Waller <rhys.bartelswaller@iohk.io>
2019-10-31 14:21:48 +1100

    Add API server availability check for integration tests

--- source/config/jest.config.ts
+++ source/config/jest.config.ts
@@ -1,6 +1,24 @@
+import gql from 'graphql-tag';
+import { RetryPromise } from 'promise-exponential-retry';
 import waitForExpect from 'wait-for-expect';
+import { apolloClient } from '../lib/graphql/apolloClient';
 
-beforeAll(() => {
-  jest.setTimeout(15000);
+beforeAll(async () => {
+  jest.setTimeout(30000);
   waitForExpect.defaults.timeout = 9000;
+  await RetryPromise.retryPromise(
+    'Checking Cardano GraphQL server is available',
+    () => {
+      return apolloClient.query({
+        query: gql`
+          query {
+            cardano {
+              blockHeight
+            }
+          }
+        `,
+      });
+    },
+    40
+  );
 });
