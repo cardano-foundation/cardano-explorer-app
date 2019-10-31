edf3f1a96dfe9a96c7faba0939b0d6c6453d9395
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-10-31 13:20:49 +0100

    [DDW-1013] Add missing search component states - blocks search result WIP

--- source/features/blocks/ui/BlocksSearchResult.tsx
+++ source/features/blocks/ui/BlocksSearchResult.tsx
@@ -1,20 +1,25 @@
 import { Observer } from 'mobx-react-lite';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { useSearchFeature } from '../../search';
 import BlockSummary from './BlockSummary';
 
 export const BlocksSearchResult = () => {
-  const { store } = useSearchFeature();
+  const { actions, store } = useSearchFeature();
+  const router = useRouter();
+  const id = router.query.id ? router.query.id.toString() : '';
+  if (id) {
+    actions.searchForBlockById.trigger({ id });
+  }
 
   return (
     <Observer>
       {() => {
-        // tslint:disable-next-line:no-console
-        console.log(store.blockSearchResult);
+        const result = store.blockSearchResult;
         return store.isSearching ? (
           <div>Is searching</div>
         ) : (
-          <BlockSummary title="Block Summary" {...store.blockSearchResult} />
+          <BlockSummary title="Block Summary" {...result} />
         );
       }}
     </Observer>
