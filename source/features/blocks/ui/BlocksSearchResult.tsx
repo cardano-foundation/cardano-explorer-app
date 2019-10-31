a0a23bede6504e19de78a2fafc3c3b7f233d501c
Dominik Guzei <dominik.guzei@gmail.com>
2019-10-31 12:18:32 +0100

    [DDW-1013] Accept searched block id by query param

--- source/features/blocks/ui/BlocksSearchResult.tsx
+++ source/features/blocks/ui/BlocksSearchResult.tsx
@@ -1,20 +1,25 @@
 import { Observer } from 'mobx-react-lite';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { useSearchFeature } from '../../search';
 import BlockSummary from './BlockSummary';
 
 export const BlocksSearchResult = () => {
-  const { store } = useSearchFeature();
-
+  const { actions, store } = useSearchFeature();
+  const router = useRouter();
+  const { query } = router;
+  if (query && query.id) {
+    const id = query.id as string;
+    actions.searchForBlockById.trigger({ id });
+  }
   return (
     <Observer>
       {() => {
-        // tslint:disable-next-line:no-console
-        console.log(store.blockSearchResult);
+        const { blockSearchResult } = store;
         return store.isSearching ? (
           <div>Is searching</div>
         ) : (
-          <BlockSummary title="Block Summary" {...store.blockSearchResult} />
+          <BlockSummary title="Block Summary" {...blockSearchResult} />
         );
       }}
     </Observer>
