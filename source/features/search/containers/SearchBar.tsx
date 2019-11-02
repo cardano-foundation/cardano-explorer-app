5ffce693f78a98708f2dec022e54c2232eb02a66
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-11-02 10:53:13 +0100

    [DDW-1014] Trigger appropriate query based on input analysis

--- source/features/search/containers/SearchBar.tsx
+++ source/features/search/containers/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { introspectAddress } from 'cardano-js/js/Address';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { BrandType } from '../../../constants';
@@ -11,20 +12,25 @@ export const SearchBar = (props: ISearchBarProps) => {
   const router = useRouter();
 
   const openSearchedPage = async (id: string) => {
-    let url;
-    if (id && id.length > 15) {
-      url = 'block?id';
-    } else if (id && id.length < 15) {
-      url = 'block?number';
-    } else if (id) {
-      url = 'epoch?number';
-    } else if (id) {
-      url = 'transaction?id';
-    }
-    if (url) {
-      router.push('/' + url + '=' + id);
-    } else {
-      router.push('/no-search-results');
+    let url = '';
+    try {
+      const { network } = introspectAddress(id);
+      if (network) {
+        url = 'transaction?id';
+      }
+    } catch (error) {
+      if (error.name !== 'InvalidAddress') {
+        if (id && id.length > 15) {
+          url = 'block?id';
+        } else if (id && id.length < 15) {
+          url = 'block?number';
+        } else if (id) {
+          url = 'epoch?number';
+        } else if (id) {
+          url = 'transaction?id';
+        }
+        router.push('/' + (url ? url + '=' + id : 'no-search-results'));
+      }
     }
   };
 
