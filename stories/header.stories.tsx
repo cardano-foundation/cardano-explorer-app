627740ceb5bd93ee00ac450e2fba6d940dbab844
Rhys Bartlels-Waller <rhys.bartelswaller@iohk.io>
2019-11-05 11:04:27 +1100

    Remove Header from Storybook as it's incompatible due to application logic coupling

--- stories/header.stories.tsx
+++ stories/header.stories.tsx
@@ -1,9 +1,9 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import { BrandType } from '../source/constants';
-import { Header } from '../source/widgets/layout';
-import { PaddingDecorator } from './support/PaddingDecorator';
-
-storiesOf('Header', module)
-  .addDecorator(story => <PaddingDecorator>{story()}</PaddingDecorator>)
-  .add('Header', () => <Header brandType={BrandType.ENLARGED} />);
+// import { storiesOf } from '@storybook/react';
+// import React from 'react';
+// import { BrandType } from '../source/constants';
+// import { Header } from '../source/widgets/layout';
+// import { PaddingDecorator } from './support/PaddingDecorator';
+//
+// storiesOf('Header', module)
+//   .addDecorator(story => <PaddingDecorator>{story()}</PaddingDecorator>)
+//   .add('Header', () => <Header brandType={BrandType.ENLARGED} />);
