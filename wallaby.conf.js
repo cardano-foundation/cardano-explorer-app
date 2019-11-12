b8034227f70f90bb55b368c68c9aa6e7d8af4e24
Dominik Guzei <dominik.guzei@gmail.com>
2019-11-12 13:18:08 +0100

    Fixes wallaby by using automatic setup detection

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,24 +1,6 @@
 module.exports = function (wallaby) {
   require('dotenv').config();
   return {
-    files: [
-      'tsconfig.json',
-      'source/**/*.ts',
-      'source/**/*.graphql',
-      '!source/**/*.spec.ts'
-    ],
-    tests: [
-      'source/**/*.spec.ts'
-    ],
-    env: {
-      type: 'node',
-    },
-    testFramework: 'jest',
-    debug: true,
-    compilers: {
-      '**/*.ts?(x)': wallaby.compilers.typeScript({
-        module: 'commonjs'
-      })
-    },
+    autoDetect: true,
   }
 };
