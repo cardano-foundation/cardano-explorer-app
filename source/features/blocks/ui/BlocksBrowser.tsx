9d95e8f7c68c00bbae5c93898060f2fd81981e6d
Dominik Guzei <dominik.guzei@gmail.com>
2019-11-19 14:29:49 +0100

    Refactor to reusable BrowseInRange component

--- source/features/blocks/ui/BlocksBrowser.tsx
+++ source/features/blocks/ui/BlocksBrowser.tsx
@@ -1,9 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import {
+  BrowseInRange,
+  IBrowseInRangeBounds,
+  IBrowseInRangeResult,
+} from '../../../widgets/browsing/BrowseInRange';
 import LoadingSpinner from '../../../widgets/loading-spinner/LoadingSpinner';
 
-import Pagination from '../../../widgets/pagination/Pagination';
+import Pagination from '../../../widgets/browsing/Pagination';
 import { useNetworkInfoFeature } from '../../network-info/context';
 import { useBlocksFeature } from '../context';
 import BlockList from './BlockList';
@@ -13,115 +18,78 @@ const BLOCKS_PER_PAGE_DEFAULT = 10;
 const BLOCKS_PER_PAGE_MINIMUM = 5;
 const BLOCKS_PER_PAGE_MAXIMUM = 30;
 
-type BrowseBlocksQueryParams = {
-  lower: number;
-  upper: number;
-};
-
-const calculateBrowseBlocksQueryParams = (
-  blockHeight: number,
-  query?: { [key: string]: string | string[] }
-): BrowseBlocksQueryParams => {
-  const isLowerParamGiven = query?.lower !== undefined;
-  const isUpperParamGiven = query?.upper !== undefined;
-  const isNoParamsGiven = !isLowerParamGiven && !isUpperParamGiven;
-  const isOnlyLowerGiven = isLowerParamGiven && !isUpperParamGiven;
-  const isOnlyUpperGiven = isUpperParamGiven && !isLowerParamGiven;
-  let lower = isLowerParamGiven ? parseInt(query?.lower as string, 10) : 0;
-  let upper = isUpperParamGiven
-    ? parseInt(query?.upper as string, 10)
-    : blockHeight;
-  if (isOnlyLowerGiven) {
-    upper = lower + BLOCKS_PER_PAGE_DEFAULT;
-  } else if (isOnlyUpperGiven || isNoParamsGiven) {
-    lower = upper - BLOCKS_PER_PAGE_DEFAULT;
-  }
-  if (upper > blockHeight) {
-    upper = blockHeight;
-  }
-  if (lower < 0) {
-    lower = 0;
-  } else if (lower > upper - BLOCKS_PER_PAGE_MINIMUM) {
-    lower = upper - BLOCKS_PER_PAGE_MINIMUM;
-  } else if (upper - lower > BLOCKS_PER_PAGE_MAXIMUM) {
-    lower = upper - BLOCKS_PER_PAGE_MAXIMUM;
-  }
-  return { lower, upper };
-};
-
-const createBrowsePath = ({ lower, upper }: BrowseBlocksQueryParams) =>
+const createBrowsePath = ({ lower, upper }: IBrowseInRangeBounds) =>
   `/browse-blocks?lower=${lower}&upper=${upper}`;
 
 const BlocksBrowser = () => {
+  const router = useRouter();
   // The network block height is required before doing any browsing
   const networkInfo = useNetworkInfoFeature();
-  const blocks = useBlocksFeature();
-  const router = useRouter();
   const { blockHeight } = networkInfo.store;
-  const isReadyToQuery = !!blockHeight;
-  let content = <LoadingSpinner />;
-  let isCorrectPath = false;
-  let browseParams: BrowseBlocksQueryParams | null = null;
-  let itemsPerPage = BLOCKS_PER_PAGE_DEFAULT;
-  let currentPage = 0;
-  let totalPages = 0;
+  const isBlockHeightAvailable = !!blockHeight;
+  const blocks = useBlocksFeature();
 
-  if (blockHeight) {
-    // Process given params
-    browseParams = calculateBrowseBlocksQueryParams(blockHeight, router.query);
-    isCorrectPath =
-      browseParams.lower.toString() === router.query?.lower &&
-      browseParams.upper.toString() === router.query?.upper;
-    itemsPerPage = browseParams.upper - browseParams.lower;
-    totalPages = Math.floor(blockHeight / itemsPerPage);
-    currentPage = Math.floor(browseParams.upper / itemsPerPage);
-  }
+  const [
+    browseParams,
+    setBrowserParams,
+  ] = useState<IBrowseInRangeResult | null>(null);
+  const isLoadingFirstTime =
+    blocks.api.getBlocksInRangeQuery.isExecutingTheFirstTime;
 
-  useEffect(() => {
-    if (!isReadyToQuery || !browseParams) {
-      return;
-    }
-    if (!isCorrectPath) {
-      // Redirect to correct query params if necessary
-      const correctedBrowsePath = createBrowsePath(browseParams);
-      router.push(correctedBrowsePath);
-    } else {
-      // If params are correct, trigger search
-      blocks.actions.browseBlocks.trigger(browseParams);
-    }
-  }, [isReadyToQuery, router.query?.lower, router.query?.upper]);
-
-  if (
-    blockHeight &&
-    !blocks.api.getBlocksInRangeQuery.isExecutingTheFirstTime
-  ) {
-    content = (
-      <>
-        <BlockList
-          isLoading={blocks.api.getBlocksInRangeQuery.isExecuting}
-          title="Browse Blocks"
-          items={blocks.store.browsedBlocks}
-        />
-        <Pagination
-          currentPage={currentPage}
-          onChangePage={(page: number) => {
-            if (page > 0 && page <= totalPages) {
-              const upper = Math.min(page * itemsPerPage, blockHeight);
-              router.push(
-                createBrowsePath({
-                  lower: upper - itemsPerPage,
-                  upper,
-                })
-              );
-            }
-          }}
-          totalPages={totalPages}
-        />
-      </>
-    );
-  }
-
-  return <div className={styles.root}>{content}</div>;
+  return (
+    <div className={styles.root}>
+      {isBlockHeightAvailable ? (
+        <>
+          <BrowseInRange
+            onReadyToBrowse={params => {
+              blocks.actions.browseBlocks.trigger(params.bounds);
+              setBrowserParams(params);
+            }}
+            onQueryParamsUpdateRequired={bounds => {
+              router.push(createBrowsePath(bounds));
+            }}
+            perPageDefault={BLOCKS_PER_PAGE_DEFAULT}
+            perPageMinimum={BLOCKS_PER_PAGE_MINIMUM}
+            perPageMaximum={BLOCKS_PER_PAGE_MAXIMUM}
+            userParamLower={router.query?.lower as string}
+            userParamUpper={router.query?.upper as string}
+            total={blockHeight}
+          />
+          {browseParams && !isLoadingFirstTime ? (
+            <>
+              <BlockList
+                isLoading={blocks.api.getBlocksInRangeQuery.isExecuting}
+                title="Browse Blocks"
+                items={blocks.store.browsedBlocks}
+              />
+              <Pagination
+                currentPage={browseParams.currentPage}
+                onChangePage={(page: number) => {
+                  if (page > 0 && page <= browseParams.totalPages) {
+                    const upper = Math.min(
+                      page * browseParams.itemsPerPage,
+                      blockHeight
+                    );
+                    router.push(
+                      createBrowsePath({
+                        lower: upper - browseParams.itemsPerPage,
+                        upper,
+                      })
+                    );
+                  }
+                }}
+                totalPages={browseParams.totalPages}
+              />
+            </>
+          ) : (
+            <LoadingSpinner />
+          )}
+        </>
+      ) : (
+        <LoadingSpinner />
+      )}
+    </div>
+  );
 };
 
 export default observer(BlocksBrowser);
