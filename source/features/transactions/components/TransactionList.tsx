ef2bc58a5161f4e3245f199674ab017c8f3190a6
Dominik Guzei <dominik.guzei@gmail.com>
2019-11-20 10:41:16 +0100

    Add back pagination for transaction lists

--- source/features/transactions/components/TransactionList.tsx
+++ source/features/transactions/components/TransactionList.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useState } from 'react';
+import Pagination from '../../../widgets/browsing/Pagination';
 import DividerWithTitle from '../../../widgets/divider-with-title/DividerWithTitle';
 import { useNavigationFeatureOptionally } from '../../navigation';
 import TransactionInfo, { ITransactionInfoProps } from './TransactionInfo';
@@ -11,17 +12,32 @@ export interface ITransactionListProps {
 }
 
 const TransactionList = (props: ITransactionListProps) => {
+  const [page, setPage] = useState(0);
   const navigation = useNavigationFeatureOptionally();
+  const handleChangePage = (newPage: number) => setPage(newPage);
+  const rowsPerPage = 2;
+  const renderedItems = props.items.slice(
+    rowsPerPage * page,
+    rowsPerPage * (page + 1)
+  );
+
   return (
     <div className={styles.transactionListContainer}>
       <div className={styles.header}>
         <DividerWithTitle title={props.title} />
       </div>
-      {props.items.map((item, index) => (
+      {renderedItems.map((item, index) => (
         <div key={`transaction_${index}`} className={styles.transactionListRow}>
           <TransactionInfo navigation={navigation?.actions} {...item} />
         </div>
       ))}
+      {props.items.length > rowsPerPage && (
+        <Pagination
+          currentPage={page}
+          totalPages={props.items.length}
+          onChangePage={handleChangePage}
+        />
+      )}
     </div>
   );
 };
