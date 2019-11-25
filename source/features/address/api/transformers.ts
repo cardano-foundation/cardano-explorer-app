fda6cee04bfed15af5d9843e747ce7d35ade807e
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2019-11-25 18:08:21 +1100

    Upgrade to cardano-js 0.3.0, adding the specificity of Cardano era to determine address format validity (#129)

--- source/features/address/api/transformers.ts
+++ source/features/address/api/transformers.ts
@@ -1,4 +1,4 @@
-import { lovelacesToAda } from 'cardano-js/dist/Currency';
+import { Currency } from 'cardano-js';
 import { SearchForAddressQuery } from '../../../../generated/typings/graphql-schema';
 import { isNotNull } from '../../../lib/types';
 import { transactionDetailsTransformer } from '../../transactions/api/transformers';
@@ -9,7 +9,9 @@ export const addressDetailTransformer = (
   s: SearchForAddressQuery
 ): IAddressDetail => ({
   address,
-  finalBalance: lovelacesToAda(s.utxos_aggregate?.aggregate?.sum?.value || '0'),
+  finalBalance: Currency.Util.lovelacesToAda(
+    s.utxos_aggregate?.aggregate?.sum?.value || '0'
+  ),
   transactions: s.transactions
     .filter(isNotNull)
     .map(transactionDetailsTransformer),
