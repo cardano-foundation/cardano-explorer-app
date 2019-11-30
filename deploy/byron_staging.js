8fb3e98835847259d9ccf7da0744b25080dc81c7
Rhys Bartlels-Waller <rhys.bartelswaller@iohk.io>
2019-11-30 22:18:44 +1100

    Remove deployment placeholder for the Byron feature scoped Incentivized Testnet, update Byron deployments protocol to http

--- deploy/byron_staging.js
+++ deploy/byron_staging.js
@@ -2,9 +2,9 @@ const deploy = require('./index');
 
 process.env.CARDANO_ERA = 'byron';
 process.env.CARDANO_NETWORK = 'mainnet';
-process.env.GRAPHQL_API_PROTOCOL = 'https';
+process.env.GRAPHQL_API_PROTOCOL = 'http';
 process.env.GRAPHQL_API_HOST = 'cardano-networks-mainnet-941840574.ap-southeast-2.elb.amazonaws.com';
-process.env.GRAPHQL_PORT = '443';
+process.env.GRAPHQL_PORT = '80';
 
 const branch = process.env.BRANCH || 'develop';
 process.env.BUCKET = `byron-staging-${branch}-explorer`;
