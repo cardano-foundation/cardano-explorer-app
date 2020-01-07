6f1878df0fea96050713391d02643b49557bf21b
Dominik Guzei <dominik.guzei@gmail.com>
2020-01-07 20:39:56 +0100

    [issue-156] Remove unused latest epochs query

--- source/features/epochs/api/index.ts
+++ source/features/epochs/api/index.ts
@@ -2,28 +2,16 @@ import { GraphQLClient } from 'graphql-request';
 import {
   GetEpochsInRangeQuery,
   GetEpochsInRangeQueryVariables,
-  GetLatestEpochsQuery,
-  GetLatestEpochsQueryVariables,
 } from '../../../../generated/typings/graphql-schema';
 import { GraphQLRequest } from '../../../lib/graphql/GraphQLRequest';
 import getEpochsInRangeQuery from './getEpochsInRange.graphql';
-import getLatestEpochsQuery from './getLatestEpochs.graphql';
 
 export class EpochsApi {
-  // https://github.com/input-output-hk/cardano-graphql/issues/52
-  public getLatestEpochsQuery: GraphQLRequest<
-    GetLatestEpochsQuery,
-    GetLatestEpochsQueryVariables
-  >;
   public getEpochsInRangeQuery: GraphQLRequest<
     GetEpochsInRangeQuery,
     GetEpochsInRangeQueryVariables
   >;
   constructor(client: GraphQLClient) {
-    this.getLatestEpochsQuery = new GraphQLRequest(
-      client,
-      getLatestEpochsQuery
-    );
     this.getEpochsInRangeQuery = new GraphQLRequest(
       client,
       getEpochsInRangeQuery
