cf9dc299b054c58d50ded00a22ed154d79bc38e9
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-03-23 18:15:10 -0600

    Fixing issue 272

--- source/widgets/browsing/Pagination.tsx
+++ source/widgets/browsing/Pagination.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Button } from 'react-polymorph/lib/components/Button';
+import { Input } from 'react-polymorph/lib/components/Input';
 import { useI18nFeature } from '../../features/i18n/context';
 import styles from './Pagination.module.scss';
 
@@ -14,10 +15,21 @@ export interface IPaginationProps {
 const Pagination = (props: IPaginationProps) => {
   const { totalPages, onChangePage, currentPage } = props;
   const { translate } = useI18nFeature().store;
+  const isFirstButtonDisabled = currentPage === 0;
+  const isLastButtonDisabled = currentPage === totalPages;
   const isLeftButtonDisabled = currentPage <= 1;
   const isRightButtonDisabled = currentPage >= totalPages;
   return (
     <div className={styles.paginationContainer}>
+      <Button
+        className={classnames([
+          styles.leftArrow,
+          isFirstButtonDisabled ? styles.disabled : null,
+        ])}
+        disabled={isFirstButtonDisabled}
+        label=""
+        onClick={() => onChangePage(0)}
+      />
       <Button
         className={classnames([
           styles.leftArrow,
@@ -28,7 +40,17 @@ const Pagination = (props: IPaginationProps) => {
         onClick={() => onChangePage(currentPage - 1)}
       />
       <div className={styles.pageInfo}>
-        <span className={styles.pageNumber}>{currentPage}</span>
+        <span className={styles.pageNumber}>
+          <Input
+            className={styles.paginationInputStyles}
+            value={currentPage.toString()}
+            onKeyPress={(e: any) => {
+              if (e.key === 'Enter') {
+                onChangePage(parseInt(e.target.value, 0));
+              }
+            }}
+          />
+        </span>
         <span className={styles.pageOf}>{translate('pagination.of')}</span>
         <span className={styles.totalPagesNumber}>{totalPages}</span>
       </div>
@@ -40,6 +62,15 @@ const Pagination = (props: IPaginationProps) => {
         disabled={isRightButtonDisabled}
         label=""
         onClick={() => onChangePage(currentPage + 1)}
+      />
+      <Button
+        className={classnames([
+          styles.rightArrow,
+          isLastButtonDisabled ? styles.disabled : null,
+        ])}
+        disabled={isLastButtonDisabled}
+        label=""
+        onClick={() => onChangePage(totalPages)}
       />
     </div>
   );
