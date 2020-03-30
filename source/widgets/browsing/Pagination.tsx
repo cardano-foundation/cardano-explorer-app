6c6351c292b5c846e18523045f35f01b6e2978b7
Dominik Guzei <dominik.guzei@gmail.com>
2020-03-30 14:02:16 +0200

    [issue-272] Fix paging input ux

--- source/widgets/browsing/Pagination.tsx
+++ source/widgets/browsing/Pagination.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import React, { Component, useRef } from 'react';
+import React, { Component, useRef, useState } from 'react';
 import { Button } from 'react-polymorph/lib/components/Button';
 import { Input } from 'react-polymorph/lib/components/Input';
 import { useI18nFeature } from '../../features/i18n/context';
@@ -14,119 +14,91 @@ export interface IPaginationProps {
   totalPages: number;
 }
 
-export default class Pagination extends Component<IPaginationProps> {
-  public inputPageNode: any = useRef<HTMLSpanElement>();
+export const Pagination = (props: IPaginationProps) => {
+  const { totalPages, onChangePage, currentPage } = props;
+  const [pageInput, setPageInput] = useState(currentPage.toString());
+  const { translate } = useI18nFeature().store;
+  const isFirstButtonDisabled = currentPage === 1;
+  const isLastButtonDisabled = currentPage >= totalPages;
+  const isLeftButtonDisabled = currentPage <= 1;
+  const isRightButtonDisabled = currentPage >= totalPages;
 
-  public componentDidUpdate() {
-    const inputNodeElement = this.inputPageNode.current.inputElement.current;
-    if (inputNodeElement) {
-      const numberOfChars = inputNodeElement.value.length;
-      if (numberOfChars <= 2) {
-        inputNodeElement.style.width = '20px';
-      } else if (numberOfChars === 3) {
-        inputNodeElement.style.width = '30px';
-      } else if (numberOfChars === 4) {
-        inputNodeElement.style.width = '40px';
-      }
-    }
-  }
-
-  public render() {
-    const { totalPages, onChangePage, currentPage } = this.props;
-    const { translate } = useI18nFeature().store;
-    const isFirstButtonDisabled = currentPage === 1;
-    const isLastButtonDisabled = currentPage >= totalPages;
-    const isLeftButtonDisabled = currentPage <= 1;
-    const isRightButtonDisabled = currentPage >= totalPages;
-
-    return (
-      <div className={styles.paginationContainer}>
-        <Button
-          className={classnames([
-            styles.leftArrow,
-            isFirstButtonDisabled
-              ? styles.disabledFirstLeftArrow
-              : styles.firstLeftArrow,
-            isFirstButtonDisabled ? styles.disabled : null,
-          ])}
-          disabled={isFirstButtonDisabled}
-          label=""
-          onClick={() => onChangePage(0)}
-        />
-        <FirstPageArrow className={styles.firstArrow} />
-        <Button
-          className={classnames([
-            styles.leftArrow,
-            isLeftButtonDisabled ? styles.disabled : null,
-          ])}
-          disabled={isLeftButtonDisabled}
-          label=""
-          onClick={() => onChangePage(currentPage - 1)}
-        />
-        <div className={styles.pageInfo}>
-          <span className={styles.pageNumber}>
-            <Input
-              ref={this.inputPageNode}
-              value={currentPage.toString()}
-              className={styles.paginationInputStyles}
-              onKeyPress={(e: any) => {
-                const targetEl = e.target;
-                if (!/^\d+$/.test(e.key)) {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  if (e.key === 'Enter') {
-                    if (targetEl.value.length <= 2) {
-                      targetEl.style.width = '20px';
-                    } else if (targetEl.value.length === 3) {
-                      targetEl.style.width = '30px';
-                    } else if (targetEl.value.length === 4) {
-                      targetEl.style.width = '40px';
-                    }
-                    if (targetEl.value && targetEl.value !== '0') {
-                      const page = parseInt(targetEl.value, 0);
-                      onChangePage(page);
-                    } else {
-                      onChangePage(currentPage);
-                    }
-                  }
-                } else {
-                  if (parseInt(targetEl.value + e.key, 0) > totalPages) {
-                    targetEl.value = targetEl.value.slice(0, -1);
-                  } else {
-                    if ((targetEl.value + e.key).length > 2) {
-                      targetEl.style.width = targetEl.offsetWidth + 10 + 'px';
-                    }
+  return (
+    <div className={styles.paginationContainer}>
+      <Button
+        className={classnames([
+          styles.leftArrow,
+          isFirstButtonDisabled
+            ? styles.disabledFirstLeftArrow
+            : styles.firstLeftArrow,
+          isFirstButtonDisabled ? styles.disabled : null,
+        ])}
+        disabled={isFirstButtonDisabled}
+        label=""
+        onClick={() => onChangePage(0)}
+      />
+      <FirstPageArrow className={styles.firstArrow} />
+      <Button
+        className={classnames([
+          styles.leftArrow,
+          isLeftButtonDisabled ? styles.disabled : null,
+        ])}
+        disabled={isLeftButtonDisabled}
+        label=""
+        onClick={() => onChangePage(currentPage - 1)}
+      />
+      <div className={styles.pageInfo}>
+        <span className={styles.pageNumber}>
+          <input
+            value={pageInput}
+            className={styles.paginationInputStyles}
+            onChange={e => {
+              setPageInput(e.currentTarget.value);
+            }}
+            onKeyPress={e => {
+              const targetEl = e.currentTarget;
+              const isNumericKey = /^\d+$/.test(e.key);
+              if (!isNumericKey) {
+                e.preventDefault();
+                e.stopPropagation();
+                if (e.key === 'Enter') {
+                  if (targetEl.value && targetEl.value !== '0') {
+                    onChangePage(parseInt(targetEl.value));
                   }
                 }
-              }}
-            />
-          </span>
-          <span className={styles.pageOf}>{translate('pagination.of')}</span>
-          <span className={styles.totalPagesNumber}>{totalPages}</span>
-        </div>
-        <Button
-          className={classnames([
-            styles.rightArrow,
-            isRightButtonDisabled ? styles.disabled : null,
-          ])}
-          disabled={isRightButtonDisabled}
-          label=""
-          onClick={() => onChangePage(currentPage + 1)}
-        />
-        <Button
-          className={classnames([
-            styles.rightArrow,
-            isLastButtonDisabled
-              ? styles.disabledSecondRightArrow
-              : styles.secondRightArrow,
-            isLastButtonDisabled ? styles.disabled : null,
-          ])}
-          disabled={isLastButtonDisabled}
-          label=""
-          onClick={() => onChangePage(totalPages)}
-        />
-        <LastPageArrow className={styles.lastArrow} />
+              }
+            }}
+            size={pageInput.length + 1}
+            maxLength={totalPages.toString().length}
+          />
+        </span>
+        <span className={styles.pageOf}>{translate('pagination.of')}</span>
+        <span className={styles.totalPagesNumber}>{totalPages}</span>
       </div>
-    );
-  }
-}
+      <Button
+        className={classnames([
+          styles.rightArrow,
+          isRightButtonDisabled ? styles.disabled : null,
+        ])}
+        disabled={isRightButtonDisabled}
+        label=""
+        onClick={() => onChangePage(currentPage + 1)}
+      />
+      <Button
+        className={classnames([
+          styles.rightArrow,
+          isLastButtonDisabled
+            ? styles.disabledSecondRightArrow
+            : styles.secondRightArrow,
+          isLastButtonDisabled ? styles.disabled : null,
+        ])}
+        disabled={isLastButtonDisabled}
+        label=""
+        onClick={() => onChangePage(totalPages)}
+      />
+      <LastPageArrow className={styles.lastArrow} />
+    </div>
+  );
+};
+
+export default Pagination;
