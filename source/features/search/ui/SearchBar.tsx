845ba6bb77cbad0df3ee0feaefea089252c8f692
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2020-08-06 20:22:37 +1000

    fix(search): Update search input introdpection to assume addresses if not hash length
    Removes address introspection, instead falling through if string is not
    a hash length
    
    Closes 342

--- source/features/search/ui/SearchBar.tsx
+++ source/features/search/ui/SearchBar.tsx
@@ -1,16 +1,10 @@
-import { Address } from 'cardano-js';
-import { AddressGroup } from 'cardano-js/dist/Address/AddressGroup';
-import { ChainSettings } from 'cardano-js/dist/ChainSettings';
 import cx from 'classnames';
 import { useState } from 'react';
 import React from 'react';
 import {
   BrandType,
-  CardanoEra,
-  CardanoNetwork,
   SearchType,
 } from '../../../constants';
-import { environment } from '../../../environment';
 import { useI18nFeature } from '../../i18n/context';
 import { useNetworkInfoFeature } from '../../network-info/context';
 import { useSearchFeature } from '../context';
@@ -30,21 +24,7 @@ export const SearchBar = (props: ISearchBarProps) => {
   const networkInfo = useNetworkInfoFeature().store;
   const introspectQuery = (query: string, type?: string) => {
     const typeOfSearch = type ? type : searchType;
-    const chainSettings =
-      environment.CARDANO.NETWORK === CardanoNetwork.MAINNET
-        ? ChainSettings.mainnet
-        : ChainSettings.testnet;
-    // Assuming the AddressGroup.jormungandr is the format for Shelley addresses
-    // Will update when final decision is made.
-    const addressGroup =
-      environment.CARDANO.ERA === CardanoEra.BYRON
-        ? AddressGroup.byron
-        : AddressGroup.jormungandr;
-    if (Address.Util.isAddress(query, chainSettings, addressGroup)) {
-      search.actions.addressSearchRequested.trigger({
-        address: query,
-      });
-    } else if (query?.length === 64) {
+    if (query?.length === 64) {
       search.actions.idSearchRequested.trigger({ id: query });
     } else if (/^\d+$/.test(query)) {
       const searchNumber = parseInt(query, 10);
@@ -65,7 +45,9 @@ export const SearchBar = (props: ISearchBarProps) => {
         }
       }
     } else {
-      search.actions.unknownSearchRequested.trigger({ query });
+      search.actions.addressSearchRequested.trigger({
+        address: query,
+      });
     }
   };
 
@@ -96,8 +78,8 @@ export const SearchBar = (props: ISearchBarProps) => {
     <>
       <Search
         brandType={props.brandType}
-        onInputChange={(query) => setSearchValue(query)}
-        onSearch={(query) => introspectQuery(query)}
+        onInputChange={(query) => setSearchValue(query.trim())}
+        onSearch={(query) => introspectQuery(query.trim())}
         onRemoveSearchType={removeSearchType}
         placeholder={translate('search.placeholder') as string}
         title={translate('search.title') as string}
