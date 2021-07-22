990b9d501ce4e6d1775394ab1c3a9b8cd1b6a53c
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2021-07-22 16:07:36 +1000

    refactor: simplify immediate object returns

--- source/features/address/api/transformers.ts
+++ source/features/address/api/transformers.ts
@@ -12,38 +12,34 @@ import { IPaymentAddressSummary, IStakeAddressSummary } from '../types';
 export const paymentAddressDetailTransformer = (
   address: string,
   s: SearchForPaymentAddressQuery
-): IPaymentAddressSummary => {
-  return {
-    address,
-    finalBalance: Currency.Util.lovelacesToAda(
-      s.paymentAddresses![0]?.summary?.assetBalances[0]?.quantity || '0'
-    ),
-    tokensBalance:
-      s
-        .paymentAddresses![0]?.summary?.assetBalances?.filter(isDefined)
-        .filter(({ asset }) => asset.assetName !== 'ada')
-        .map((t) => ({
-          ...t,
-          asset: assetTransformer(t.asset as Asset)
-        }))
-        .sort(sortTokensDesc) || [],
-    transactionsCount:
-      s.transactions_aggregate?.aggregate?.count.toString() || '0',
-  };
-};
+): IPaymentAddressSummary => ({
+  address,
+  finalBalance: Currency.Util.lovelacesToAda(
+    s.paymentAddresses![0]?.summary?.assetBalances[0]?.quantity || '0'
+  ),
+  tokensBalance:
+    s
+      .paymentAddresses![0]?.summary?.assetBalances?.filter(isDefined)
+      .filter(({ asset }) => asset.assetName !== 'ada')
+      .map((t) => ({
+        ...t,
+        asset: assetTransformer(t.asset as Asset)
+      }))
+      .sort(sortTokensDesc) || [],
+  transactionsCount:
+    s.transactions_aggregate?.aggregate?.count.toString() || '0',
+});
 
 export const stakeAddressDetailTransformer = (
   address: string,
   s: SearchForStakeAddressQuery
-): IStakeAddressSummary => {
-  return {
-    address,
-    totalWithdrawals:
-      s.withdrawals_aggregate?.aggregate?.count.toString() || '0',
-    totalWithdrawn: Currency.Util.lovelacesToAda(
-      s.withdrawals_aggregate?.aggregate?.sum?.amount || '0'
-    ),
-    transactionsCount:
-      s.transactions_aggregate?.aggregate?.count.toString() || '0',
-  };
-};
+): IStakeAddressSummary => ({
+  address,
+  totalWithdrawals:
+    s.withdrawals_aggregate?.aggregate?.count.toString() || '0',
+  totalWithdrawn: Currency.Util.lovelacesToAda(
+    s.withdrawals_aggregate?.aggregate?.sum?.amount || '0'
+  ),
+  transactionsCount:
+    s.transactions_aggregate?.aggregate?.count.toString() || '0',
+});
